c859db0952be877b354e9e57e0c2d4bfa1ec7a64
Girum Deressa <girumderessa@gmail.com>
2022-03-31 03:31:23 -0700

    returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,14 +8,14 @@
  */
 int is_prime(unsigned int n, unsigned int c)
 {
-  if (n % c == 0)
-    {
-      if (n == c)
-	return (1);
-      else
-	return (0);
-    }
-  return (0 + is_prime(n, c + 1));
+if (n % c == 0)
+{
+if (n == c)
+return (1);
+else
+return (0);
+}
+return (0 + is_prime(n, c + 1));
 }
 /**
  * is_prime_number - detects if an input number is a prime number.
@@ -32,3 +32,4 @@ if (n == 1)
 return (0);
 return (is_prime(n, 2));
 }
+
